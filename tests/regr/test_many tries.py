ea8a8befa9c11dc22ddcf96d637d515937abdb54
root <root@perftestappsvm.kqr04dgcsmnedikxjlhkhijddd.parx.internal.cloudapp.net>
2021-10-11 13:16:29 +0000

    add try test

---
+++ tests/regr/test_many tries.py
@@ -0,0 +1,49 @@
+try:
+    try:
+        try:
+            try:
+                try:
+                    try:
+                        try:
+                            try:
+                                try:
+                                    try:
+                                        try:
+                                            try:
+                                                try:
+                                                    try:
+                                                        try:
+                                                            try:
+                                                                raise "o͡͡͡͡͡͡͡͡͡͡͡͡͡͡╮(｡❛ᴗ❛｡)╭o͡͡͡͡͡͡͡͡͡͡͡͡͡͡  I won't help, it works for me" 
+                                                            except:
+                                                                raise ValueError
+                                                        except:
+                                                            raise TypeError 
+                                                    except:
+                                                        raise ArithmeticError 
+                                                except:
+                                                    raise BufferError 
+                                            except:
+                                                raise LookupError 
+                                        except:
+                                            raise AssertionError 
+                                    except:
+                                        raise AttributeError 
+                                except:
+                                    raise EOFError 
+                            except:
+                                raise FloatingPointError 
+                        except:
+                            raise GeneratorExit 
+                    except:
+                        raise ImportError 
+                except:
+                    raise ModuleNotFoundError 
+            except:
+                raise IndexError 
+        except:
+            raise KeyError 
+    except:
+        raise KeyboardInterrupt 
+except:
+    raise MemoryError
